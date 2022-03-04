dfbfc214848b0f5b6ba14a75bbcc6e8ebcf17fd8
techicode <techicode@outlook.com>
2022-03-04 05:29:50 -0300

    added v1 route prefix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const api = require('./routes/api');
 
 const app = express();
 
@@ -13,8 +12,6 @@ app.use(
 );
 
 app.use(express.json());
-
-app.use('/launches', launchesRouter);
-app.use('/planets', planetsRouter);
+app.use('/v1', api);
 
 module.exports = app;
